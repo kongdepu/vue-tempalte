ce02af20f480d53a6be59489e25781d6b0bbeb92
kongdepu <kongdepu@126.com>
2022-01-04 16:57:58 +0800

    init

---
+++ src/views/index.vue
@@ -0,0 +1,25 @@
+<template>
+<div>开始吧</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+  props:{},
+  watch: {},
+  computed: {},
+  filters: {},
+  components: {},
+  created() {},
+  mounted() {
+    console.log(process.env.NODE_ENV)
+  },
+  methods: {
+  },
+};
+</script>
+<style scoped></style>
